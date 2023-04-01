d9fb9ba79115d5b9b4d1d3894ce7e997ff56d0ac
SK-LODHI <128723824+SK-LODHI@users.noreply.github.com>
2023-04-01 11:46:05 +0500

    Update 2048 the game.cpp

--- 2048 the game.cpp
+++ 2048 the game.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+
 using namespace std;
 
 const int size = 4; // this will deterine the size of the game. in this case its 4x4
@@ -9,62 +10,61 @@ const int MAX_VALUE = 2048; // this will be the highest tile
 int board[size][size]; // making a 2d array
 int score = 0;
 int highScore = 0;
+
 // functions
-void printBoard() ;
-
+void printBoard();
 void clearBoard();
-
-void addRandomTile() ;
-   
-
-void slideTiles(int direction) ;
-
-bool isGameOver() ;
+void addRandomTile();
+void moveUp();
+void moveDown();
+void moveLeft();
+void moveRight();
+bool isGameOver();
 
 int main() {
-	cout<<"WELCOME TO 2048"<<endl;
-srand(time(NULL));
-clearBoard();
-addRandomTile();
-addRandomTile();
-printBoard();
-while (true) {
-char input;
-cout << "Enter move (W = up, A = left, S = down, D = right, Q = quit): "; // this allows user to play the game
-cin >> input;
-if (input == 'W' || input == 'w') { // casses for the inputs enetred by the user
-slideTiles(0);
-} else if (input == 'D' || input == 'd') {
-slideTiles(1);
-} else if (input == 'S' || input == 's') {
-slideTiles(2);
-} else if (input == 'A' || input == 'a') {
-slideTiles(3);
-} else if (input == 'Q' || input == 'q') {
-	cout << "Final Score: " << score << endl;
-break;
-} else {
-cout << "Invalid input!" << endl;
-continue;
-}
-if (isGameOver()) { // prints the highscore and final score when the game is over
-cout << "Game Over!" << endl;
-if (score > highScore) {
-highScore = score;
-}
-cout << "Final Score: " << score << endl;
-cout << "High Score: " << highScore << endl;
-break;
-}
-printBoard();
-}
-return 0;
+    cout << "WELCOME TO 2048" << endl;
+    srand(time(NULL));
+    clearBoard();
+    addRandomTile();
+    addRandomTile();
+    printBoard();
+    while (true) {
+        char input;
+        cout << "Enter move (W = up, A = left, S = down, D = right, Q = quit): "; // this allows user to play the game
+        cin >> input;
+        if (input == 'W' || input == 'w') { // casses for the inputs enetred by the user
+            moveUp();
+        } else if (input == 'D' || input == 'd') {
+            moveRight();
+        } else if (input == 'S' || input == 's') {
+            moveDown();
+        } else if (input == 'A' || input == 'a') {
+            moveLeft();
+        } else if (input == 'Q' || input == 'q') {
+            cout << "Final Score: " << score << endl;
+            break;
+        } else {
+            cout << "Invalid input!" << endl;
+            continue;
+        }
+        if (isGameOver()) { // prints the highscore and final score when the game is over
+            cout << "Game Over!" << endl;
+            if (score > highScore) {
+                highScore = score;
+            }
+            cout << "Final Score: " << score << endl;
+            cout << "High Score: " << highScore << endl;
+            break;
+        }
+        printBoard();
+    }
+    return 0;
 }
 
 void printBoard() { // this function print the board on which you play the game
     cout << "Score: " << score << "   High Score: " << highScore << endl;
     // formatting (i know its bad :) )
-    cout << "|------|------|------|------|"<< endl;
+    cout << "|------|------|------|------|" << endl;
     for (int i = 0; i < size; i++) {
         cout << "|";
         for (int j = 0; j < size; j++) {
@@ -77,6 +77,7 @@ void printBoard() { // this function print the board on which you play the game
         cout << endl << "|------|------|------|------|" << endl;
     }
 }
+
 void clearBoard() { // this sets all the elements of the array to 0 or none
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
@@ -88,99 +89,151 @@ void clearBoard() { // this sets all the elements of the array to 0 or none
 void addRandomTile() { // for adding random numbers in a random tile
     int count = 0;
     for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            if (board[i][j] == 0) {
-                count++;
-            }
-        }
-    }
-    if (count == 0) {
-        return;
-    }
-    int index = rand() % count;
-    int value = (rand() % 2 + 1) * 2; // either 2 or 4
-    count = 0;
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            if (board[i][j] == 0) {
-                if (count == index) {
-                    board[i][j] = value;
-                    return;
-                }
-                count++;
-            }
-        }
-    }
-}
-
-void slideTiles(int direction) {
-	// this function not only moves the tiles but also adds the number along the way
-    bool moved = false;
-    int rowOffset = 0, colOffset = 0;
-    switch (direction) {
-    	// indexing
-        case 0: // up
-            rowOffset = -1;
-            break;
-        case 1: // right
-            colOffset = 1;
-            break;
-        case 2: // down
-            rowOffset = 1;
-            break;
-        case 3: // left
-            colOffset = -1;
-            break;
-    }
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            int row = i, col = j;
-            if (board[row][col] == 0) {
-                continue;
-            }
-            while (true) { // for adding the tiles if they can be added 
-                int nextRow = row + rowOffset, nextCol = col + colOffset;
-                if (nextRow < 0 || nextRow >= size || nextCol < 0 || nextCol >= size) {
-                    break;
-                }
-                if (board[nextRow][nextCol] == 0) {
-                    board[nextRow][nextCol] = board[row][col];
-                    board[row][col] = 0;
-                    row = nextRow;
-                    col = nextCol;
-                    moved = true;
-                } else if (board[nextRow][nextCol] == board[row][col]) {
-                    board[nextRow][nextCol] += board[row][col];
-score += board[nextRow][nextCol];
-board[row][col] = 0;
-moved = true;
-break;
-} else {
-break;
+        for (int j =0; j < size; j++) {
+if (board[i][j] == 0) { // checks if the tile is empty or not
+count++; // if the tile is empty increment the counter
 }
 }
 }
+if (count == 0) { // if there are no empty tiles, exit the function
+return;
 }
-if (moved) {
-addRandomTile();
-}
-}
-
-bool isGameOver() { // this checks whether the game is over or not by itrating over every tile,
-// this also checks for empty spaces or tiles containing same numbers
+int randomIndex = rand() % count; // generates a random number between 0 and count-1
+count = 0; // reset the count
 for (int i = 0; i < size; i++) {
 for (int j = 0; j < size; j++) {
-if (board[i][j] == 0) {
-return false;
+if (board[i][j] == 0) { // again checks if the tile is empty or not
+if (count == randomIndex) { // if the count is equal to the random number generated before
+board[i][j] = (rand() % 2 + 1) * 2; // generates either 2 or 4 randomly
+return;
 }
-if (j < size - 1 && board[i][j] == board[i][j+1]) {
-return false;
-}
-if (i < size - 1 && board[i][j] == board[i+1][j]) {
-return false;
+count++; // increment the count for each empty tile
 }
 }
 }
-return true;
+}
+void moveUp() {
+    bool hasMoved = false;
+    for (int j = 0; j < size; j++) {
+        for (int i = 1; i < size; i++) {
+            if (board[i][j] == 0) continue;
+            int k = i - 1;
+            while (k >= 0 && board[k][j] == 0) k--;
+            if (k == -1) {
+                board[0][j] = board[i][j];
+                board[i][j] = 0;
+                hasMoved = true;
+            } else if (board[k][j] == board[i][j]) {
+                board[k][j] *= 2;
+                score += board[k][j];
+                if (score > highScore) highScore = score;
+                board[i][j] = 0;
+                hasMoved = true;
+            } else if (k + 1 != i) {
+                board[k + 1][j] = board[i][j];
+                board[i][j] = 0;
+                hasMoved = true;
+            }
+        }
+    }
+    if (hasMoved) addRandomTile();
 }
 
+void moveDown() {
+    bool hasMoved = false;
+    for (int j = 0; j < size; j++) {
+        for (int i = size - 2; i >= 0; i--) {
+            if (board[i][j] == 0) continue;
+            int k = i + 1;
+            while (k < size && board[k][j] == 0) k++;
+            if (k == size) {
+                board[size - 1][j] = board[i][j];
+                board[i][j] = 0;
+                hasMoved = true;
+            } else if (board[k][j] == board[i][j]) {
+                board[k][j] *= 2;
+                score += board[k][j];
+                if (score > highScore) highScore = score;
+                board[i][j] = 0;
+                hasMoved = true;
+            } else if (k - 1 != i) {
+                board[k - 1][j] = board[i][j];
+                board[i][j] = 0;
+                hasMoved = true;
+            }
+        }
+    }
+    if (hasMoved) addRandomTile();
+}
+
+void moveLeft() {
+    bool hasMoved = false;
+    for (int i = 0; i < size; i++) {
+        for (int j = 1; j < size; j++) {
+            if (board[i][j] == 0) continue;
+            int k = j - 1;
+            while (k >= 0 && board[i][k] == 0) k--;
+            if (k == -1) {
+                board[i][0] = board[i][j];
+                board[i][j] = 0;
+                hasMoved = true;
+            } else if (board[i][k] == board[i][j]) {
+                board[i][k] *= 2;
+                score += board[i][k];
+                if (score > highScore) highScore = score;
+                board[i][j] = 0;
+                hasMoved = true;
+            } else if (k + 1 != j) {
+                board[i][k + 1] = board[i][j];
+                board[i][j] = 0;
+                hasMoved = true;
+            }
+        }
+    }
+    if (hasMoved) addRandomTile();
+}
+
+void moveRight() {
+bool hasMoved = false;
+for (int i = 0; i < size; i++) {
+for (int j = size - 2; j >= 0; j--) {
+if (board[i][j] == 0) continue;
+int k = j + 1;
+while (k < size && board[i][k] == 0) k++;
+if (k == size) {
+board[i][size - 1] = board[i][j];
+board[i][j] = 0;
+hasMoved = true;
+} else if (board[i][k] == board[i][j]) {
+board[i][k] *= 2;
+score += board[i][k];
+if (score > highScore) highScore = score;
+board[i][j] = 0;
+hasMoved = true;
+} else if (k - 1 != j) {
+board[i][k - 1] = board[i][j];
+board[i][j] = 0;
+hasMoved = true;
+}
+}
+}
+if (hasMoved) addRandomTile();
+}
+
+
+bool isGameOver() { // checks if the game is over or not
+for (int i = 0; i < size; i++) {
+for (int j = 0; j < size; j++) {
+if (board[i][j] == 0) { // if there is an empty tile, the game is not over
+return false;
+}
+if (j < size - 1 && board[i][j] == board[i][j + 1]) { // if there are two adjacent tiles with the same value, the game is not over
+return false;
+}
+if (i < size - 1 && board[i][j] == board[i + 1][j]) { // if there are two adjacent tiles with the same value, the game is not over
+return false;
+}
+}
+}
+return true; // if none of the above conditions are true, the game is over
+}
